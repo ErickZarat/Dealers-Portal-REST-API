7838aa2607596920427405d6220079189d17bc8c
Erick Zarat <ezarat15@gmail.com>
2021-06-14 07:15:37 -0600

    improve cors config

--- src/main/java/me/erickzarat/portal/config/CorsConfig.java
+++ src/main/java/me/erickzarat/portal/config/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT","DELETE");
             }
         };
     }
